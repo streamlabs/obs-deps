ef002530af8dee6fbb44a00dab0fad738e2b4925
Vladimir Sumarov <summeroff@gmail.com>
2026-08-22 00:52:00 +0200

    scripts: Remove unnecessary comments

--- deps.ffmpeg/99-ffmpeg.ps1
+++ deps.ffmpeg/99-ffmpeg.ps1
@@ -93,12 +93,7 @@ function Configure {
         '--disable-sdl2'
         '--disable-doc'
         '--disable-postproc'
-        # Unused by OBS and a repeat source of decoder CVEs (CVE-2026-8461, CVE-2026-66039)
         '--disable-decoder=magicyuv,mace3,mace6'
-        # Software HEVC decode. Policy since 2022, lost in the FFmpeg 7.0 bump
-        # (2024-05-08sl2). hevc_cuvid is deliberately kept: it is pure NVDEC, and
-        # without it NVIDIA hosts lose HEVC playback entirely, since every other
-        # hardware path is a hwaccel on the software decoder. Encoders unaffected.
         '--disable-decoder=hevc'
         $(if ( ! $script:Shared ) { ('--pkg-config-flags=' + "'--static'") })
         $(if ( $Configuration -eq 'Debug' ) { '--enable-debug' } else { '--disable-debug' })
